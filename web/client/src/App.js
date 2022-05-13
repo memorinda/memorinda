2f05a66b5ac48ef8849aa3d0ab919e358caf654e
Mert Ture <mertture0@gmail.com>
2022-05-13 21:53:44 +0300

    implement login page

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Navigate } from "react-router";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./pages/error-page/error-page.jsx";
+import Login from "./pages/login/login.jsx";
 import Signup from "./pages/signup/signup.jsx";
 import { useStore } from "./store/store";
 
@@ -11,16 +14,42 @@ function App() {
   return (
     <React.Suspense fallback={<div>Loading...</div>} >
       <Routes>
-          <>    
+        {!currentUser ?
+          <>
+            <Route
+              path="/login"
+              element={<Login />}
+            />
             <Route
               path="/signup"
               element={<Signup />}
             />
             <Route
               path="*"
-              element={<Signup />}
+              element={<Login />}
+            />
+          </> :
+          <>
+      
+            <Route
+              path="/"
+              element={<Login />}
+            />
+
+            <Route
+              path="/login"
+              element={<Navigate to="/home"/>}
+            />
+            <Route
+              path="/signup"
+              element={<Navigate to="/home"/>}
+            />
+            <Route
+              path="*"
+              element={<ErrorPage />}
             />
           </>
+        }
       </Routes>
     </React.Suspense>
   );
---
+++ web/client/src/pages/login/login.jsx
@@ -0,0 +1,129 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { useStore } from "../../store/store";
+import { userLogin } from "../../store/userReducer";
+import "./login.css";
+
+const loginSchema = z
+  .object({
+
+    email: z.string().email("Please enter a valid email"),
+    password: z.string().nonempty("Password required."),
+  });
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+    mode: "all",
+  });
+
+  const [, dispatch] = useStore();
+
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+
+  const onSubmit = useCallback((data) => {
+    const user = {
+      email: data.email,
+      password: data.password,
+    };
+    console.log(user);
+    axios
+      .post(`${process.env.REACT_APP_URL}/users/login/${user.email}`, { data: { user: user } })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200 && res.data.message) {
+          setErrorMessage(res.data.message);
+        } else if (res.status === 200) {
+          setErrorMessage("You logged in succesfully");
+          const dbUser = res.data;
+          console.log("login user:", dbUser);
+          dispatch(userLogin(dbUser));
+          navigate("/home");
+        } else {
+          setErrorMessage("Error! Please try again.");
+        }
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+        setErrorMessage("Error! Please try again.");
+      });
+  }, [navigate, dispatch]);
+  return (
+    <div className="imge">
+      <div className="fullscreen row justify-content-center align-items-center">
+        <div className="col-4 justify-content-start">
+          <div className="card p-1 mb-0">
+            <div className="card-body">
+              <div className="text-center">
+                <h2 className="mt-2 mb-3">
+                  <b>SIGN IN</b>
+                </h2>
+              </div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <p className="errorMessage">{errorMessage}</p>
+                <div className="mt-4 d-flex flex-column">
+                  <input
+                    {...register("email")}
+                    className="btn-border input-style form-control"
+                    placeholder="E-mail"
+                    type="email"
+                  >
+                  </input>
+                  <small className="align-self-start error-text">
+                    {errors.email?.message}
+                  </small>
+                </div>
+                <div className="mt-3 d-flex flex-column">
+                  <input
+                    {...register("password")}
+                    className="btn-border input-style form-control"
+                    placeholder="Password"
+                    type="password"
+                  >
+                  </input>
+                  <small className="align-self-start error-text">
+                    {errors.password?.message}
+                  </small>
+                </div>
+
+                <div className="mt-5 row text-center justify-content-center">
+                  <button
+                    type='submit'
+                    className="col-6 btn btn-block btn-warning"
+                  >
+                      SIGN IN
+                  </button>
+                </div>
+                <div className="mt-3 row text-center justify-content-center">
+                  <div className="col-12">
+                    <span
+                      className="link-line-gap d-flex justify-content-center"
+                    >
+                    Don&apos;t have an account?
+                      <Link to="/signup">
+                        <p>   Create one!</p>
+                      </Link>
+                    </span>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
