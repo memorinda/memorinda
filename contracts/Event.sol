849c14e9711de9a8eae548654531491192422305
Orekk12 <kurnazkerem@sabanciuniv.edu>
2022-05-14 12:53:37 +0300

    Eventfactory, event, ticketfactory, ticket, TODO ticketfactory and ticket not finished

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -1,50 +1,77 @@
 pragma solidity ^0.4.17;
 //https://soliditydeveloper.com/ecrecover TODO
 
+contract EventFactory{
+
+    address[] public deployedEvents;
+
+    function createEvent() public {
+        address newEvent = new Event();
+        deployedEvents.push(newEvent);
+    }
+
+    function getDeployedEvents() public view returns(address[]) {
+        return deployedEvents;
+    }
+
+}
+
 contract Event{
+
+    string public _caption;
+    uint public _id;
+    uint public _ticketAmount;
+    address public _managerAddress;
+
+    Ticket[] public _ticketList;
+
     struct Ticket 
     {
-        address  manager;
-        uint ticketCost;
-        address owner;
-        bool onSale;
-        uint ticketID;
-        uint ticketHash;
+        uint _ticketID;
+        uint _eventID;
+        address _manager;
+        address _owner;
+        uint _ticketCost;
+        bool _onSale;
+        //uint ticketHash;
     }
 
-    string public caption;
-    uint public eventID;
-    uint public ticketAmount;
-    address public managerAddress;
-    Ticket[] public ticketList;
-
-    function Event(string _caption, uint _eventID, uint _ticketAmt, uint _ticketCost) public 
+    //name description capacity eventdate location price 
+    function Event(string caption, uint ticketAmt, uint ticketCost, address creator) public 
     {
-        caption = _caption;
-        eventID = _eventID;
-        ticketAmount = _ticketAmt;
-        managerAddress = msg.sender;
+        _caption = caption;
+        //createEventId()
+        _ticketAmount = ticketAmt;
+        _managerAddress = creator;
 
         //create tickets
-        for (uint i = 0; i < _ticketAmt; i++) {
-            createTicket(_ticketCost, managerAddress, i);
+        if(_ticketAmount > 0)
+        {
+            for (uint i = 0; i < _ticketAmount; i++) {
+                createTicket(ticketCost, _managerAddress, i, _id);
+            }
         }
+        
     }
 
-    // to create ticket
-    function createTicket(uint _ticketCost, address _manager, uint id) 
+    /*modifier restricted() {
+        require(msg.sender == _owner, "Error: Cannot change object properties, wrong owner");
+        _;
+    }*/
+
+    function createTicket(uint ticketCost, address manager, uint ticketID, uint eventID) private//TODO change id counter
     {
         Ticket memory newTicket = Ticket({
-            manager: _manager,
-            ticketCost: _ticketCost,
-            owner: _manager,
-            onSale: true,
-            ticketID: id,//?
-            ticketHash: 111
+            _ticketID: id,
+            _eventID: eventID,
+            _manager: manager,//owner is manager of the vent at ticket creation
+            _owner: manager,
+            _ticketCost: ticketCost,
+            _onSale: true
         });
 
         //create hash and id
-        ticketList.push(newTicket);
+        _ticketList.push(newTicket);
     }
 
     /*function createTicketID(){
@@ -55,38 +82,84 @@ contract Event{
         return 111;
     }*/
 
-    function buy_ticket(uint _ticketID) public payable
+    /*function buy_ticket(uint ticketID) public payable
     {
         Ticket foundTicket;
-        for (uint i = 0; i < ticketAmount; i++) {
-            if(ticketList[i].ticketID == _ticketID)
+        for (uint i = 0; i < _ticketAmount; i++) {
+            if(_ticketList[i]._ticketID == ticketID)
             {
-                foundTicket = ticketList[i];
+                foundTicket = _ticketList[i];
             }
         }
 
-        require(foundTicket.onSale == true, "Error: Ticket is not on sale.");
-        require(msg.value == foundTicket.ticketCost, "Error: Ticket payment is not equal to ticket cost.");
+        require(foundTicket._onSale == true, "Error: Ticket is not on sale.");
+        require(msg.value == foundTicket._ticketCost, "Error: Ticket payment is not equal to ticket cost.");
 
-        foundTicket.owner = msg.sender;
+        foundTicket._owner = msg.sender;
     }
 
-    function setTicketSale(bool saleFlag, uint _ticketID)
+    function setTicketSale(bool saleFlag, uint ticketID) public restricted
     {
         Ticket foundTicket;
-        for (uint i = 0; i < ticketAmount; i++) {
-            if(ticketList[i].ticketID == _ticketID)
+        for (uint i = 0; i < _ticketAmount; i++) {
+            if(_ticketList[i]._ticketID == ticketID)
             {
-                foundTicket = ticketList[i];
+                foundTicket = _ticketList[i];
             }
         }
 
-        require(foundTicket.owner == msg.sender, "Error: Cannot change ticket sale state, wrong");
+        //require(foundTicket.owner == msg.sender, "Error: Cannot change ticket sale state, wrong user");//same as restricted
         foundTicket.onSale = saleFlag;
-    }
+    }*/
 
 }
+
+contract TicketFactory{
+    address[] public deployedTickets;
+}
     
+contract Ticket{
+
+    string public _eventCaption
+    uint public _id;
+    uint public _eventID;
+    address public _eventManager;
+    address public _owner;
+    uint public _cost;
+    bool public _onSale;
+    
+    modifier restricted() {
+        require(msg.sender == _owner, "Error: Cannot change object properties, wrong owner");
+        _;
+    }
+
+    function Ticket(string eventCaption, uint eventID, uint cost) public restricted{
+        _eventCaption = eventCAption;
+        _eventID = eventID;
+        _cost = cost;
+
+        //set id
+        
+    }
+
+    modifier restricted() {
+        require(msg.sender == _owner, "Error: Cannot change ticket sale state, wrong user");
+        _;
+    }
+    function buy_ticket() public payable
+    {
+        require(_onSale == true, "Error: Ticket is not on sale.");
+        require(msg.value == _cost, "Error: Ticket payment is not equal to ticket cost.");
+
+        _owner = msg.sender;
+    }
+
+    function setTicketSale(bool saleFlag) public restricted
+    {
+        //require(_owner == msg.sender, "Error: Cannot change ticket sale state, wrong user");//restriced checks it
+        _onSale = saleFlag;
+    }
+}
 
 
 
