4daa2e2fe0151c415835eb79b458e973e9c6d901
Orekk12 <kurnazkerem@sabanciuniv.edu>
2022-06-25 23:07:00 +0300

    added image link upload

--- ethereum/contracts/EventFactory.sol
+++ ethereum/contracts/EventFactory.sol
@@ -104,9 +104,8 @@ contract Event is ERC721URIStorage {
         uint _ticketCost;
         bool _onSale;
         bool _isActive;
-        string _ticketURI;
-        string _ticketOwnerURI;
-        string[] _imageLinks;
+        string[] _ownerImageLinks;
+        string[] _organizerImageLinks;
     }
 
     //name description capacity eventdate location price
@@ -125,14 +124,14 @@ contract Event is ERC721URIStorage {
         _;
     }
 
-    function createTicketsByAmount(string memory tokenURI, uint ticketCost, uint ticketAmount, string[] imageLinks) public restricted{
+    function createTicketsByAmount(string memory tokenURI, uint ticketCost, uint ticketAmount, string[] memory imageLinks) public restricted{
         for (uint i = 0; i < ticketAmount; i++) {
             createTicket(tokenURI, ticketCost, imageLinks);
         }
     }
 
     //create a single ticket
-    function createTicket(string memory tokenURI, uint ticketCost, string[] imageLinks) public {
+    function createTicket(string memory tokenURI, uint ticketCost, string[] memory imageLinks) public {
         _ticketIds.increment();
         uint256 newTokenId = _ticketIds.current();
         Ticket memory newTicket = Ticket({
@@ -143,9 +142,8 @@ contract Event is ERC721URIStorage {
             _ticketCost: ticketCost,
             _onSale: true,
             _isActive: true,
-            _ticketURI: tokenURI,
-            _ticketOwnerURI: "",
-            _imageLinks: imageLinks
+            _ownerImageLinks: new string[](0),
+            _organizerImageLinks: imageLinks
         });
         //tokenURI = Strings.toString(newTicket._ticketID);//tokenURI
         _mint(msg.sender, newTokenId);
@@ -209,9 +207,8 @@ contract Event is ERC721URIStorage {
             _ticketCost: 0,
             _onSale: false,
             _isActive: false,
-            _ticketURI: "",
-            _ticketOwnerURI: "",
-            _imageLinks: []
+            _ownerImageLinks: new string[](0),
+            _organizerImageLinks: new string[](0)
             });
             return newTicket;
         }
@@ -334,11 +331,18 @@ contract Event is ERC721URIStorage {
         return idToTicket[tID]._owner;
     }
 
-    function uploadTicketURI(uint ticketID, string memory ticketURI) public
-    {
-        require(idToTicket[ticketID]._owner == msg.sender, "Error: Cannot change ticket sale state, wrong user");//restriced checks it
+    function uploadOwnerImage(uint ticketID, string[] memory imageLinks) public {
+        require(idToTicket[ticketID]._owner == msg.sender, "Error: Cannot change ticket image state, wrong user");//restriced checks it
 
-        idToTicket[ticketID]._ticketOwnerURI = ticketURI;
+        for (uint256 i=0; i< imageLinks.length; i++) 
+            idToTicket[ticketID]._ownerImageLinks.push(imageLinks[i]);
+    }
+
+    function uploadOrganizerImage(uint ticketID, string[] memory imageLinks) public {
+        require(idToTicket[ticketID]._owner == msg.sender, "Error: Cannot change ticket image state, wrong user");//restriced checks it
+
+        for (uint256 i=0; i< imageLinks.length; i++) 
+            idToTicket[ticketID]._organizerImageLinks.push(imageLinks[i]);
     }
 
     /*
